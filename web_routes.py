9fe734d839e5b528ff31eb5dea1d339f3b292d20
Rajas Rasam <33662891+rasamrajas@users.noreply.github.com>
2020-03-28 17:14:42 +0530

    Update web_routes.py

--- web_routes.py
+++ web_routes.py
@@ -20,9 +20,9 @@ import logging
 from telethon.tl.custom import Message
 from aiohttp import web
 
-from .util import unpack_id, get_file_name, get_requester_ip
-from .config import request_limit
-from .telegram import client, transfer
+from util import unpack_id, get_file_name, get_requester_ip
+from config import request_limit
+from telegram import client, transfer
 
 log = logging.getLogger(__name__)
 routes = web.RouteTableDef()
