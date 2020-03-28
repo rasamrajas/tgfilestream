6430a576bdace73e60955c6612635d2c3cb78eb9
Rajas Rasam <33662891+rasamrajas@users.noreply.github.com>
2020-03-28 17:13:35 +0530

    Update log.py

--- log.py
+++ log.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import logging
 
-from .config import log_config, debug
+from config import log_config, debug
 
 if log_config:
     logging.basicConfig(filename=log_config)
